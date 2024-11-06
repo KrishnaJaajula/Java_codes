f1c3ac0ce18040c48e0c59b7dbdeef3d2653cd85
KrishnaJaajula <jmohankrishna379@gmail.com>
2024-11-06 19:12:43 +0530

    finding the sum of N natural numbers

---
+++ SumN.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class SumN{
+
+public static void main(String[] args){
+
+      Scanner obj = new Scanner(System.in);
+      int a = obj.nextInt();
+      int b = 0;
+
+   for(int i=0; i<=a; i++){
+
+       b += i;
+
+   System.out.println(b);
+
+       }
+     }
+    }
